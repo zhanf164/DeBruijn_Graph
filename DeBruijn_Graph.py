79b28a507f3d3d87645da7f414a6aedf0581a842
Zach <zach@Zachs-MBP.lan1>
2020-01-25 14:58:51 -0500

    Added a function for graphing the DeBruijn graph, and cleaned up some functions. Need to implement walk through graph.

--- DeBruijn_Graph.py
+++ DeBruijn_Graph.py
@@ -6,7 +6,9 @@ Created on Fri Jan 24 22:48:06 2020
 @author: zach
 """
 
-
+import networkx as nx
+import matplotlib.pyplot as plt
+from graphviz import Digraph
 
 class DeBruijnGraph():
     
@@ -14,12 +16,13 @@ class DeBruijnGraph():
         self.nodes = []
         self.edges = {}
         
+        
     
     
     def Generate_Kmers(self, reads, klength):
         kmers = []
         for read in reads:
-            for i in range(len(read)-klength):
+            for i in range((len(read)-klength)+1):
                 kmers.append(read[i:i+klength])
                 
         return kmers    
@@ -33,10 +36,10 @@ class DeBruijnGraph():
             else:
                 self.nodes.append(leftmer)
             if rightmer in self.nodes:
-                passsfo
+                pass
             else:
                 self.nodes.append(rightmer)
-            if leftmer in self.edges():
+            if leftmer in self.edges:
                 self.edges[leftmer].append(rightmer)
             else:
                 self.edges[leftmer] = [rightmer]
@@ -44,3 +47,20 @@ class DeBruijnGraph():
     
     def EulerianWalk(self, nodes, edges):
         pass
+    
+    
+    
+    
+def DrawGraph(DeBruijnObj):
+    new_edges = []
+    for key, value in DeBruijnObj.edges.items():
+        for val in value:
+            new_edges.append((key, val))
+    dot = Digraph()
+    for node in DeBruijnObj.nodes:
+        dot.node(node)
+    dot.edges(new_edges)
+    dot.render(view=True)
+    
+
+    
